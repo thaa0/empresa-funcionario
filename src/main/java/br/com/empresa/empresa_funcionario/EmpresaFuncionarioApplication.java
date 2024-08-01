7b9f7eeb174fe163e8ef3cb45710407cb544ea2a
Thalita Souza <thalitaoliveira917@gmail.com>
2024-08-01 17:00:21 -0300

    inicia projeto

---
+++ src/main/java/br/com/empresa/empresa_funcionario/EmpresaFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.empresa.empresa_funcionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaFuncionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/empresa/empresa_funcionario/EmpresaFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.empresa_funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
